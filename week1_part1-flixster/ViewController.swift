c9f60627f1b0c0143569692441d7a59c5f48742e
laminesunga <78405929+laminesunga@users.noreply.github.com>
2021-08-29 23:21:26 -0700

    Initial Commit

---
+++ week1_part1-flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week1_part1-flixster
+//
+//  Created by Undergrad-Staff on 8/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
